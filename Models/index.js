5600054ba7e1ce8fe89021b6e87bcf09c5bee5fd
Rama <ramirocarnicersouble8@gmail.com>
2024-05-24 23:04:45 -0300

    Revert "Models and db changed to SQL"
    This reverts commit 25aaf636437f1e686ff7a4f749e7726fead1a3f1.

--- Models/index.js
+++
@@ -1,7 +0,0 @@
-import { UrlModel } from './urlSchema.js';
-import { User } from './user.js';
-
-User.hasMany(UrlModel, { foreignKey: {name:'userId'} })
-UrlModel.belongsTo(User, { foreignKey: {name:'userId'} })
-
-export { User, UrlModel };
--- Models/user.js
+++
@@ -1,6 +0,0 @@
-import sequelize from '../Config/db.js';
-import { DataTypes } from 'sequelize';
-
-export const User = sequelize.define('user' , {
-  authId: DataTypes.STRING,
-});
