bd9ffd64f0c1ac150712cc94857b84f979cc167a
Rama <ramirocarnicersouble8@gmail.com>
2024-07-25 23:06:09 -0300

    [fix] Added code 404 when no url found

--- Controller/urlController.js
+++ Controller/urlController.js
@@ -1,36 +1,34 @@
-import { findUrl, validateData, createShortUrl, updateClicks } from '../Services/urlService.js';
+import { findUrl, validateData, createShortUrl, updateClicks } from '../Services/urlService.js'
 import { findUser } from '../Services/authService.js'
-import { DatabaseError } from '../Utils/errors.js';
-//import { deleteCache } from '../Utils/redis.js';
+import { DatabaseError } from '../Utils/errors.js'
+// import { deleteCache } from '../Utils/redis.js';
 
 export class UrlController {
-
-  static async createShortUrl(req, res){
-    const isValid = validateData(req.body);
-    if(!isValid.success) return res.status(400).send({ message: isValid });
-    console.log(req.body.url);
-    console.log(req.token);
-    if(req.token != undefined){
-      const userExist = await findUser(req.userId);
-      if(userExist == '') return res.status(400).send('The user not have any links');
-      console.log(userExist);
-      req.id = userExist.id;
+  static async createShortUrl (req, res) {
+    const isValid = validateData(req.body)
+    if (!isValid.success) return res.status(400).send({ message: isValid })
+    console.log(req.body.url)
+    console.log(req.token)
+    if (req.token != undefined) {
+      const userExist = await findUser(req.userId)
+      if (userExist == '') return res.status(404).send('The user not have any links')
+      console.log(userExist)
+      req.id = userExist.id
     }
-    console.log(req.id);
+    console.log(req.id)
 
-    const shortU = await createShortUrl(req.body.url, req.id);
-    if(shortU instanceof DatabaseError) return res.status(500).send({ message: shortU.message });
-    
-    return res.status(200).send({data: shortU});
+    const shortU = await createShortUrl(req.body.url, req.id)
+    if (shortU instanceof DatabaseError) return res.status(500).send({ message: shortU.message })
+
+    return res.status(200).send({ data: shortU })
   }
 
   static async getOriginalUrl (req, res) {
-    const url = await findUrl(req.params.id);
-    if(url == '') return res.status(404).json({  message: 'Error: Id not found'});
-    if(url instanceof DatabaseError) return res.status(500).json({ message: url.message });
-    
-    await updateClicks(req.params.id, url.clicks);
-    res.redirect(url.originalUrl);
-  }
+    const url = await findUrl(req.params.id)
+    if (url == '') return res.status(404).json({ message: 'Error: Id not found' })
+    if (url instanceof DatabaseError) return res.status(500).json({ message: url.message })
 
+    await updateClicks(req.params.id, url.clicks)
+    res.redirect(url.originalUrl)
+  }
 }
